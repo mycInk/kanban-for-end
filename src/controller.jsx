26c4b918227d04dbbb5b121bc9b0519a1c112fdd
myc_Ink <231250142@smail.nju.edu.cn>
2024-08-05 13:53:05 +0800

    test

--- src/controller.jsx
+++ src/controller.jsx
@@ -1,14 +1,26 @@
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Link, Routes } from 'react-router-dom';
 import SignIn from './signin';
 import SignUp from './signup';
-const Controller = () => {
-  return (
-    <div>
-      <Router>
-        <Route path="signin" Component={SignIn} />
-        <Route path="signup" Component={SignUp} />
-      </Router>
-    </div>
-  );
-};
+    const Controller = () => {
+      return (
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/signin">SignIn</Link>
+                </li>
+                <li>
+                  <Link to="/signup">SignUp</Link>
+                </li>
+              </ul>
+            </nav>
+            <Routes>
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/signup" element={<SignUp />} />
+            </Routes>
+          </div>
+        </Router>
+      );
+    };
 export default Controller;
