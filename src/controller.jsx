6d4e774878f2a9a4d0f454651c4d341a0924d83b
myc_Ink <231250142@smail.nju.edu.cn>
2024-08-05 14:27:50 +0800

    del link

--- src/controller.jsx
+++ src/controller.jsx
@@ -1,20 +1,10 @@
-import { Router, Route, Link, Routes } from 'react-router-dom';
+import { Router, Route, Routes } from 'react-router-dom';
 import SignIn from './signin';
 import SignUp from './signup';
     const Controller = () => {
       return (
         <Router>
           <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/signin">SignIn</Link>
-                </li>
-                <li>
-                  <Link to="/signup">SignUp</Link>
-                </li>
-              </ul>
-            </nav>
             <Routes>
               <Route path="/signin" element={<SignIn />} />
               <Route path="/signup" element={<SignUp />} />
